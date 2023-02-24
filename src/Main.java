8dad2e6abb8673f6360c4bf0b5fe6dca044350a1
CristianUsuga <usugahiguitacristian@gmail.com>
2023-02-24 06:34:39 -0500

    Modiicaciones

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import co.edu.poli.ces3.Estudiante;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Estudiante pedro = new Estudiante();
+        pedro.age = 4;
+
+
     }
 }
--- src/co/edu/poli/ces3/Estudiante.java
+++ src/co/edu/poli/ces3/Estudiante.java
@@ -1,15 +1,17 @@
 package co.edu.poli.ces3;
 
 public class Estudiante {
-    public static void main(String[] args) {
 
-        System.out.println("Hola");
+    public int age;
+    private  boolean isMarried;
+    protected float c;
+    char p;
+   //alcance dentro del objeto.
 
-    }
+
     public static int sumar(int a, int b){
         return a+b;
     }
-
     public static int resta(int a, int b){
         return a-b;
     }
